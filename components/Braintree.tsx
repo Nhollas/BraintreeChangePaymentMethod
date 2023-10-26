6fe157e671da97a402776dcb99eb7d88dc6e5ef5
CodeSandbox <support@codesandbox.io>
2023-10-26 13:44:25 +0000

    Sandbox snapshot commit

--- components/Braintree.tsx
+++ components/Braintree.tsx
@@ -2,87 +2,83 @@
 
 import axios from "axios";
 import braintree, { Dropin } from "braintree-web-drop-in";
-import clsx from "clsx";
 
-import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 export default function Braintree({
   clientToken,
-  hideBraintree
+  hideBraintree,
 }: {
   clientToken: string;
   hideBraintree: Dispatch<SetStateAction<boolean>>;
 }) {
-  const buttonRef = useRef<HTMLButtonElement | null>(null);
-
-  const [dropinInstance, setDropinInstance] = useState<Dropin | undefined>(undefined);
-  const [requestable, setRequestable] = useState(0);
-
-  console.log("requestable", requestable)
+  const [dropinInstance, setDropinInstance] = useState<Dropin | undefined>(
+    undefined,
+  );
+  const [requestable, setRequestable] = useState(false);
 
   const [returnedCard, setReturnedCard] = useState({});
 
   useEffect(() => {
-    async function initializeBraintree() {
-      if (dropinInstance) {
-        await dropinInstance.teardown()
-        setDropinInstance(undefined)
-      }
+    if (clientToken === undefined) return;
 
-     const instance = await braintree.create(
-        {
+    async function initializeBraintree() {
+      try {
+        if (dropinInstance) {
+          await dropinInstance.teardown();
+          setDropinInstance(undefined);
+        }
+
+        const instance = await braintree.create({
           authorization: clientToken,
           container: "#dropin-container",
           dataCollector: true,
-        },
-      );
+        });
 
-      instance?.on("paymentMethodRequestable", () => {
-        console.log("paymentMethodRequestable");
-        setRequestable((prev) => prev + 1);
-      });
+        instance?.on("paymentMethodRequestable", () => {
+          console.log("paymentMethodRequestable");
+          setRequestable(true);
+        });
 
-      instance?.on("changeActiveView", (d) => {
-        console.log("changeActiveView", d)
-      })
+        instance?.on("noPaymentMethodRequestable", () => {
+          console.log("noPaymentMethodRequestable");
+          setRequestable(false);
+        });
 
-      instance?.on("noPaymentMethodRequestable", () => {
-        console.log("noPaymentMethodRequestable");
-        setRequestable(0);
-      });
-
-      setDropinInstance(instance)
+        setDropinInstance(instance);
+      } catch (error) {}
     }
 
-    initializeBraintree();
-  }, [clientToken, dropinInstance]);
+    if (clientToken) {
+      initializeBraintree();
+    }
+  }, [clientToken]);
 
   useEffect(() => {
-    console.log("We are going to try and send request")
-    console.log("requestable", requestable)
-    console.log("dropinInstance", dropinInstance)
-    if (requestable !== 0 && dropinInstance) {
-      console.log("We are going to send a request")
-      dropinInstance.requestPaymentMethod(
-      async function (requestPaymentMethodErr, payload) {
-        if (requestPaymentMethodErr) {
-          console.log("requestPaymentMethodErr", requestPaymentMethodErr);
-          return;
-        }
+    console.log("We are going to try and send request");
+    console.log("requestable", requestable);
+    console.log("dropinInstance", dropinInstance);
+    if (requestable && dropinInstance) {
+      console.log("Sending request");
+      dropinInstance
+        .requestPaymentMethod()
+        .then(async (payload) => {
+          const requestBody = {
+            nonce: payload.nonce,
+            deviceData: payload.deviceData,
+          };
 
-        const requestBody = {
-          nonce: payload.nonce,
-          deviceData: payload.deviceData
-        }
+          const response = await axios.put(
+            "https://zkf2h5-3000.csb.app/api/customer/card",
+            requestBody,
+          );
 
-        const response = await axios.put("https://zkf2h5-3000.csb.app/api/customer/card", requestBody)
+          setReturnedCard(response.data);
 
-        setReturnedCard(response.data);
-
-        console.log("response", response);
-      },
-      )
+          console.log("response", response);
+        })
+        .catch((error) => console.log(error));
     }
-  }, [requestable, dropinInstance])
+  }, [requestable]);
 
   return (
     <section className="w-full h-full">
@@ -94,16 +90,15 @@ export default function Braintree({
       </button>
 
       <div id="dropin-container"></div>
-      <button
+      {/* <button
         //disabled={!requestable}
         className={clsx(
           //!requestable && "bg-gray-500",
           "bg-blue-600 px-6 py-2 rounded-lg mt-6",
         )}
-        ref={buttonRef}
       >
         Change Card
-      </button>
+      </button> */}
       <pre className="text-sm mt-6">
         {JSON.stringify(returnedCard, null, 4)}
       </pre>
